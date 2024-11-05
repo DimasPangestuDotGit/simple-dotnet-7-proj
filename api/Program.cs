cec6bfa948a9f7f982503a2ab2d678836512d4a4
Dimas Pangestu <Dimas.Pangestu.MBC@bni.co.id>
2024-11-05 22:35:55 +0700

    .net core GET and create dbContext

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,21 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDBContext"), sqlOptions =>
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null
+        )
+    ));
 
 var app = builder.Build();
 
@@ -15,4 +28,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 app.Run();
